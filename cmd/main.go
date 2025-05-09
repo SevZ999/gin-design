3fbeb1bd49175e6c8f5b93645a6c1214c4e97c3a
SevZ999 <767372425@qq.com>
2025-05-10 00:35:46 +0800

    dev

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"gin-design/internal/app"
 	"gin-design/internal/config"
+
+	"github.com/google/wire"
 )
 
 func main() {
 	cfg := config.NewConfig()
 
 	app := app.NewApp(cfg.Init())
+
+	wire.StructProvider
 
 	app.Run()
 }
