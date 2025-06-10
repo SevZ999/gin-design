f9f30344a4a7fff6a4183a1dd644f73d2d715568
sevz98 <sevz98@163.com>
2025-06-11 04:43:30 +0800

    delete config test

--- internal/config/config_test.go
+++ internal/config/config_test.go
@@ -1,15 +1,10 @@
 package config
 
-import (
-	"log"
-	"testing"
-)
+// func TestLoadConfig(t *testing.T) {
+// 	config, err := LoadConfig("dev")
+// 	if err != nil {
+// 		t.Fatal(err)
+// 	}
 
-func TestLoadConfig(t *testing.T) {
-	config, err := LoadConfig("dev")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	log.Printf("%+v", config)
-}
+// 	log.Printf("%+v", config)
+// }
