4c38625049585088cea2864f4957c1cb33f80787
sevz98 <sevz98@163.com>
2025-06-11 19:34:16 +0800

    ?

--- internal/app/controller/user.go
+++ internal/app/controller/user.go
@@ -19,12 +19,13 @@ func NewUserController(userService *service.UserService) *UserController {
 
 func (c *UserController) GetUser(ctx *gin.Context) {
 
-	resp, err := c.srv.GetUser(GetCtx(ctx), dto.GetUserReq{Id: 200})
-	if err != nil {
-		ctx.JSON(200, dto.Error(
-			1,
-			err.Error(),
-		))
-	}
-	ctx.JSON(200, dto.Success(resp))
+	// resp, err := c.srv.GetUser(GetCtx(ctx), dto.GetUserReq{Id: 200})
+	// if err != nil {
+	// 	ctx.JSON(200, dto.Error(
+	// 		1,
+	// 		err.Error(),
+	// 	))
+	// }
+	// ctx.JSON(200, dto.Success(resp))
+	ctx.JSON(200, dto.Success("this is canary"))
 }
